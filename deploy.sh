8a7ac9fcd6dcc976d7e4d7499e1eefadaf7135e2
szimano <szimano@szimano.org>
2013-12-15 20:08:15 +0100

    deploy separate script and allow run parameters for tomcat

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 . $DIR/env.sh
 
-$DIR/stop.sh
-
 rm -rf $TOMCAT_HOME/webapps/ROOT*
 
 cp $WAR_FILE $TOMCAT_HOME/webapps
-
-$DIR/start.sh
 
--- start.sh
+++ start.sh
@@ -4,5 +4,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 . $DIR/env.sh
 
+if ! [[ -z "$1" ]]
+then
+	export JAVA_OPTS="$1"
+fi
+
 $TOMCAT_HOME/bin/catalina.sh start
 
