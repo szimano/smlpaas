372d2494521a5bde58e7b978102df0be8608fb9f
szimano <szimano@szimano.org>
2013-12-17 12:23:35 +0100

    wait for response message when sending chat msgs

--- archipel-client/src/main/groovy/com/softwaremill/smlpaas/ArchipelClient.groovy
+++ archipel-client/src/main/groovy/com/softwaremill/smlpaas/ArchipelClient.groovy
@@ -1,9 +1,12 @@
 package com.softwaremill.smlpaas
 
 import com.softwaremill.smlpaas.packets.ArchipelPacket
+import org.jivesoftware.smack.Chat
 import org.jivesoftware.smack.Connection
+import org.jivesoftware.smack.MessageListener
 import org.jivesoftware.smack.Roster
 import org.jivesoftware.smack.XMPPConnection
+import org.jivesoftware.smack.packet.Message
 
 class ArchipelClient extends Thread {
 
@@ -39,10 +42,61 @@ class ArchipelClient extends Thread {
             case "list":
                 listVMs(conn)
                 break
+            case "start":
+                startVM(conn, args.drop(3))
+                break
+            case "stop":
+                stopVM(conn, args.drop(3))
+                break
             default:
                 println "Unknown command: ${args[2]}"
                 break
         }
+    }
+
+    static def startVM(Connection conn, String[] args) {
+        if (args.length != 1) {
+            println "Usage USERNAME PASSWORD start VM_NAME"
+        }
+
+        sendMessageTo(conn, "start", args[0])
+    }
+
+    static def stopVM(Connection conn, String[] args) {
+        if (args.length != 1) {
+            println "Usage USERNAME PASSWORD stop VM_NAME"
+        }
+
+        sendMessageTo(conn, "stop", args[0])
+    }
+
+    static def sendMessageTo(Connection conn, String message, String vmName) {
+        def userID = findVMByName(conn, vmName)
+
+        def chatManager = conn.getChatManager()
+        def chat = chatManager.createChat(userID, new MessageListener() {
+            @Override
+            void processMessage(Chat chat, Message msg) {
+                // we do not really care
+                println("message from ${chat.participant}: ${msg.body}")
+                shouldRun = false
+            }
+        })
+
+        chat.sendMessage(message)
+
+        new ArchipelClient().start()
+    }
+
+    static def findVMByName(Connection conn, String name) {
+        def id
+
+        conn.getRoster().entries.each {if (it.name == name) id = it.user}
+
+        if (id == null)
+            throw new RuntimeException("Could not find VM of name ${name}")
+
+        return id
     }
 
     static void cloneVM(Connection conn, String[] args) {
